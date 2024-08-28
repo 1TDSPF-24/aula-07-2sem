129d0f9a5cb792127912d1f25c43fa9733eb8d89
joaobroggine <joaovitorbrogginelopes@gmail.com>
2024-08-28 11:47:53 -0300

    Utilizando styled-components.

--- aula07/src/routes/Error/index.tsx
+++ aula07/src/routes/Error/index.tsx
@@ -1,13 +1,23 @@
+import styled from "styled-components";
 
+const img404 = styled.img`
+  display: block;
+  margin: 0, auto;
+  width: 50%
+`;
 
 export default function Error(){
 
       //MUDANDO O TÍTULO DA PÁGINA!!!
       document.title = "Erro 404";
-
+      let nrAleatorio = Math.floor(Math.random() * 3) + 1;
     return(
       <div>
-        <h1>Olá, mundo sou o Error!</h1>
+        <h1>Erro 404</h1>
+        <h1>Página não encontrada.</h1>
+        <figure>
+          <img src={`/404/404-${nrAleatorio}.avif`} alt="Error 404" />
+        </figure>
       </div>
     );
   }
