16faa4c36ce9e99f1568a0d97c39b04b1aaea526
cadenasza <leonardocadena2005@gmail.com>
2024-08-28 12:02:12 -0300

    fianlizando

--- aula07/src/routes/Error/index.tsx
+++ aula07/src/routes/Error/index.tsx
@@ -1,13 +1,24 @@
+import styled from "styled-components";
 
+const Img404 = styled.img`
+  display: block;
+  margin: 0 auto;
+  width: 50%;
+`
 
 export default function Error(){
 
       //MUDANDO O TÍTULO DA PÁGINA!!!
       document.title = "Erro 404";
 
+      const nrAleatorio = Math.floor(Math.random()*3) + 1;
     return(
       <div>
-        <h1>Olá, mundo sou o Error!</h1>
+        <h1>Erro 404</h1>
+        <h2>Pagina nao encotrada</h2>
+        <figure>
+          <Img404 src={`/img-404-${nrAleatorio}.jpg`} alt="Erro 404" />
+        </figure>
       </div>
     );
   }
