1f2d30534c2e6adaed76a90d3aebb569153ca12b
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2024-08-28 11:13:59 -0300

    Passagem de parametros com useParams e criando uma lista de produtos.

---
+++ aula07/src/listaProdutos.ts
@@ -0,0 +1,69 @@
+//Criando um array de objetos de produtos eletrônicos com os seguintes atributos:
+//id, nome, qtd, preco, cor, imagem
+
+export let listaProdutos:{id:number,nome:string,qtd:number,preco:number,cor:string,imagem:string}[] = [
+  {
+    id: 1,
+    nome: "Smartphone",
+    qtd: 10,
+    preco: 1500.0,
+    cor: "Preto",
+    imagem: "smartphone.jpg",
+  },
+  {
+    id: 2,
+    nome: "Tablet",
+    qtd: 5,
+    preco: 800.0,
+    cor: "Branco",
+    imagem: "tablet.jpg",
+  },
+  {
+    id: 3,
+    nome: "Notebook",
+    qtd: 3,
+    preco: 2500.0,
+    cor: "Preto",
+    imagem: "notebook.jpg",
+  },
+  {
+    id: 4,
+    nome: "Impressora",
+    qtd: 2,
+    preco: 300.0,
+    cor: "Branco",
+    imagem: "impressora.jpg",
+  },
+  {
+    id: 5,
+    nome: "Fone de Ouvido",
+    qtd: 15,
+    preco: 200.0,
+    cor: "Preto",
+    imagem: "fone.jpg",
+  },
+  {
+    id: 6,
+    nome: "Câmera",
+    qtd: 8,
+    preco: 1200.0,
+    cor: "Branco",
+    imagem: "camera.jpg",
+  },
+  {
+    id: 7,
+    nome: "Relógio",
+    qtd: 12,
+    preco: 800.0,
+    cor: "Preto",
+    imagem: "relógio.jpg",
+  },
+  {
+    id: 8,
+    nome: "Bolsa",
+    qtd: 18,
+    preco: 500.0,
+    cor: "Branco",
+    imagem: "bolsa.jpg",
+  },
+];
