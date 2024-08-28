ecef5b4c1a090515dbc4a994a3d1950b1b11ff8e
joaobroggine <joaovitorbrogginelopes@gmail.com>
2024-08-28 11:15:02 -0300

    Feito os detructions e as listas

--- aula07/src/routes/Produtos/index.tsx
+++ aula07/src/routes/Produtos/index.tsx
@@ -1,4 +1,4 @@
-
+import { listaProdutos } from "../listaProdutos";
 
 export default function Produtos(){
 
@@ -7,7 +7,40 @@ export default function Produtos(){
 
     return(
       <div>
-        <h1>Olá, mundo sou o Produtos!</h1>
+        <h1>Produtos eletronicos</h1>
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Nome</th>
+                <th>Quantidade</th>
+                <th>Preço</th>
+                <th>Cor</th>
+                <th>Foto</th>
+              </tr>
+            </thead>
+            <tbody>
+                {listaProdutos.map((produto)=>(
+                  <tr key={produto.id}>
+                      <td>{produto.id}</td>
+                      <td>{produto.nome}</td>
+                      <td>{produto.qtd}</td>
+                      <td>{produto.preco}</td>
+                      <td>{produto.cor}</td>
+                      <td>{produto.imagem}</td>
+                  </tr>
+                ))}
+            </tbody>
+            <tfoot>
+              <tr>
+                <td colSpan={6}>
+                  Quantidade de produtos atual: {listaProdutos.length}
+                </td>
+              </tr>
+            </tfoot>
+          </table>
+        </div>
       </div>
     );
   }
